0d9b8f1490629d6c1e9c9da6dd94be0e303c9416
tony fraser <afraser@hurricane.lan>
2023-07-23 22:48:53 -0400

    lus

---
+++ bridge/R/final_project.R
@@ -0,0 +1,10 @@
+Question 1 - 
+
+lf = 'https://raw.githubusercontent.com/tonythor/cuny-datascience/develop/bridge/R/data/lusitania_crew_manifest.csv'
+tf = 'https://raw.githubusercontent.com/tonythor/cuny-datascience/develop/bridge/R/data/titanic_lifeboats.csv'
+
+titanic <- read.csv(tf)
+lusitania <- read.csv(lf)
+
+titanic
+lusitania 
